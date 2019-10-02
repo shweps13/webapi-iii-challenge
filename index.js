3278f78e65380e83ec26a439bd216192bf78b950
Heorhii Siburov <g.siburov@gmail.com>
2019-10-02 13:30:34 -0700

    All CRUD operations are working

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = express();
 server.use(express.json());
 
 
-// === CRUD operations here ===
+// === CRUD operations here ========================
 server.get('/', (req, res) => {
     res.send(`
       <h2>Heorhii API</h>
@@ -92,7 +92,77 @@ server.put('/api/users/:id', (req, res) => {
         res.status(500).json({ error: "The user information could not be updated." })
     });
 });
-// === End of CRUD operations ===
+// End of Users operations
+
+// ==================================================
+
+// Posts operations
+server.get('/api/posts', (req, res) => {
+
+    postDB.get()
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    });
+
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    postDB.getById(id)
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    });
+
+});
+
+server.post('/api/posts/:id', (req, res) => {
+    const postInfo = req.body;
+
+    postDB.insert(postInfo)
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post information could not be added." })
+    });
+});
+//waiting for object like { "user_id": 1,"text": "Olololo" }
+
+server.delete('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+
+    postDB.remove(id)
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post information could not be deleted." })
+    });
+
+});
+
+server.put('/api/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+
+    postDB.update(id, update)
+    .then(posts => {
+        res.json(posts);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The post information could not be updated." })
+    });
+});
+// End of Posts operations
+
+// === End of CRUD operations ======================
 
 
 const port = 5050;
