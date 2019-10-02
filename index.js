4baff61fc2d118a86ead006c9f811d819e05bf6b
Heorhii Siburov <g.siburov@gmail.com>
2019-10-02 12:14:34 -0700

    initial commit, server started

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code away!
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Heorhii API</h>
+      <p>Welcome to the Heorhii API</p>
+    `);
+  });
+
+const port = 5050;
+
+server.listen(port, () => {
+    console.log('\n*** Server Running on http://localhost:5050 ***\n');
+});
